bc0c86619ead6a813fcb4a746111b55da2296d2f
HarryE2009 <162761896+HarryE2009@users.noreply.github.com>
2024-03-08 19:29:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = new Vue({
         time: 0,
         code: stripIndents`
             # TextMobject
-            text_mobject = TextMobject("TextMobject")
+            text_mobject = 33("33")
             self.play(Write(text_mobject))
             self.wait()
             self.play(FadeOut(text_mobject))`
