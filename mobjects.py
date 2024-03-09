5fd8002ada59f0520566c4254aa2025a4dbb579d
HarryE2009 <162761896+HarryE2009@users.noreply.github.com>
2024-03-08 16:46:13 -0800

    Update mobjects.py

--- mobjects.py
+++ mobjects.py
@@ -7,7 +7,7 @@ class Mobjects(Scene):
     '''
     def construct(self):
         # TextMobject
-        text_mobject = TextMobject("TextMobject")
+        text_mobject = TextMobject("34342342342")
         self.play(Write(text_mobject))
         self.wait()
         self.play(FadeOut(text_mobject))
